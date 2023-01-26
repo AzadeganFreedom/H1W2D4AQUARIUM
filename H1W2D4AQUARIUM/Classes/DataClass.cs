a6956ba1d1dca9bddb078bcfb7addb32bb6344a5
Max Jacobsen <mumse999@outlook.dk>
2023-01-26 11:14:37 +0100

    Col update

--- H1W2D4AQUARIUM/Classes/DataClass.cs
+++ H1W2D4AQUARIUM/Classes/DataClass.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Text.Json;
 namespace H1W2D4AQUARIUM.Classes
 {
     internal class DataClass
     {
+        public Fishclass Fish;
+        public AquariumClass Aquarium;
         public void PrepareProgram()
         {
             if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "Fish.dat"))
@@ -24,7 +26,25 @@ namespace H1W2D4AQUARIUM.Classes
         }
         public void LoadData()
         {
+            string jsonData = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "Fish.dat");
+            if (!string.IsNullOrWhiteSpace(jsonData))
+            {
+                Fish.FishLists = JsonSerializer.Deserialize<List<Fishclass.FishObject>>(jsonData);
+            }
+            jsonData = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "Aq.dat");
+            if (!string.IsNullOrWhiteSpace(jsonData))
+            {
+                Aquarium.AquariumList = JsonSerializer.Deserialize<List<AquariumClass.AquariumObject>>(jsonData);
 
+            }
+
+        }
+        public void SaveData()
+        {
+            string jsonData = JsonSerializer.Serialize(Fish.FishLists);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "Fish.dat", jsonData);
+            jsonData = JsonSerializer.Serialize(Aquarium.AquariumList);
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "Aq.dat", jsonData);
         }
     }
 }
